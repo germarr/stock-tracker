c42e38a983625c64ffce03160e2345278c5daefe
Gerardo Martínez Arroyo <you@example.com>
2021-08-23 23:03:47 -0500

    Database created

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+import models 
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+from pydantic import BaseModel
+
+
+class StockRequest(BaseModel):
+    symbol:str
 
 templates = Jinja2Templates(directory="templates")
 
+
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
 @app.get("/hello")
 def read_root():
     return {"Hello": "World"}
@@ -15,5 +27,17 @@ def index(request:Request):
     Display the main Dashboard from Youtube.
     """
     return templates.TemplateResponse("dashboard.html",{
-        "request":request
+        "request":request,
+        "somevar":2
     })
+
+@app.post("/stock")
+def create_stock(stock_request:StockRequest):
+    """
+    Create a Stock and store it into the database.
+    """
+
+    return{
+        "code":"success",
+        "message":"stock created"
+    }
